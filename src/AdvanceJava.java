509186b29d677745b45e77f06f5e2a2cbbedd6f4
2177Shubham <singh2177shubham@gmail.com>
2025-09-20 14:38:27 +0530

    new change

--- src/AdvanceJava.java
+++ src/AdvanceJava.java
@@ -1,8 +1,115 @@
+import java.util.Scanner;
 public class AdvanceJava {
-    public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-        int c = a + b;
-        System.out.println(c);
+
+    public class statment {
+        public static void main(String[] args) {
+
+  /*
+   //EVEN And odd
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the value of a");
+
+    int a = sc.nextInt();
+    if (a %2== 0) {
+        System.out.println("a is EVEN");
+    } else {
+        System.out.println("a is ODD");
     }
-}
+
+
+
+
+   // largest of two numbers
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the value of a");
+    int a = sc.nextInt();
+    Scanner sc1 = new Scanner(System.in);
+    System.out.println("Enter the value of b");
+    int b = sc1.nextInt();
+    if (a > b) {
+        System.out.println("a is greater than b");
+    } else if (a < b) {
+        System.out.println("b is greater than a");
+    } else {
+        System.out.println("a is equal to b");
+    }
+
+
+
+    //income tax calculator
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the income");
+        int income = sc.nextInt();
+        int tax;
+        if(income < 500000){
+            tax = 0;
+
+        }
+        else if(income >= 500000 && income < 1000000){
+            tax= (int) (0.2 * income);
+
+        }
+        else{
+            tax= (int)(0.3 * income);
+
+            }
+              System.out.println("the tax is " + tax);
+        }
+
+
+
+
+
+
+       largest of three numbers
+          Scanner sc = new Scanner (System.in);
+          System.out.println("enter the value of a");
+          int a = sc.nextInt();
+          Scanner sc1 = new Scanner(System.in);
+          System.out.println("enter the value of b");
+          int b = sc1  .nextInt();
+            Scanner sc2 = new Scanner(System.in);
+            System.out.println("enter the value of c");
+            int c = sc2.nextInt();
+            if (a>=b && a>=c)
+            {
+                System.out.println("a is the largest");
+            }
+            else if (b>=a && b>=c)
+            {
+                System.out.println("b is the largest");
+            }
+            else
+            {
+                System.out.println("c is the largest");
+
+            }
+
+
+
+
+
+
+
+
+
+        Scanner sc = new  Scanner(System.in);
+        System.out.println("enter the number ");
+        int number = sc .nextInt();
+
+        String type = (number % 2 == 0) ? "even" : "odd";
+        System.out.println( type);*/
+
+            //pass or fail
+            Scanner sc = new Scanner(System.in);
+            System.out.println("enter the marks of student ");
+            int marks = sc.nextInt();
+            if (marks >= 33) {
+                System.out.println("pass");
+            } else {
+                System.out.println("fail");
+            }
+
+        }}}
+
+
